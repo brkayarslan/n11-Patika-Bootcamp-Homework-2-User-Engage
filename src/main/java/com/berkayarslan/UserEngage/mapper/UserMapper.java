25e57477e31506520b1384acd7c4450696524a84
Berkay Arslan <berkayarslan9@gmail.com>
2024-02-19 13:53:43 +0300

    development dto and mapper

--- src/main/java/com/berkayarslan/UserEngage/mapper/UserMapper.java
+++ src/main/java/com/berkayarslan/UserEngage/mapper/UserMapper.java
@@ -2,14 +2,24 @@ package com.berkayarslan.UserEngage.mapper;
 
 import com.berkayarslan.UserEngage.dto.UserDTO;
 import com.berkayarslan.UserEngage.model.User;
+import com.berkayarslan.UserEngage.request.UserSaveRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
+import java.util.List;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
 
     UserDTO userToUserDTO(User user);
 
     User userDTOToUser(UserDTO userDTO);
+
+    User userDTOToUser(UserSaveRequest request);
+
+    List<UserDTO> userListToUserDTOList(List<User> userList);
 }
